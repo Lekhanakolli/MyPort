a4938a32e3c4029aa6369e44822c7e8eb28011c9
Lekhana Kolli <kollilekhanareddy@gmail.com>
2026-03-18 13:09:26 +0530

    first upload

---
+++ script.js
@@ -0,0 +1,12 @@
+// Dark mode toggle
+const btn = document.getElementById('darkBtn');
+
+btn.addEventListener('click', function() {
+  document.body.classList.toggle('dark-mode');
+  
+  if (document.body.classList.contains('dark-mode')) {
+    btn.textContent = '☀️ Light Mode';
+  } else {
+    btn.textContent = '🌙 Dark Mode';
+  }
+});
